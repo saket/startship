7c1efed94bdbf22cf211d25e3471a7e81b25dc9e
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-10-11 16:57:46 +0200

    Add username and password Gradle properties as command options

--- src/main/kotlin/nevam/User.kt
+++ src/main/kotlin/nevam/User.kt
@@ -4,12 +4,18 @@ import nevam.util.GradleProperties
 
 data class NexusUser(val username: String, val password: String) {
   companion object {
-    fun readFrom(fileName: String): NexusUser {
+    fun readFrom(
+        fileName: String,
+        usernameProperty: String = DEFAULT_USERNAME_PROPERTY, passwordProperty: String = DEFAULT_PASSWORD_PROPERTY
+    ): NexusUser {
       val properties = GradleProperties(fileName)
       return NexusUser(
-          username = properties["SONATYPE_NEXUS_USERNAME"],
-          password = properties["SONATYPE_NEXUS_PASSWORD"]
+          username = properties[usernameProperty],
+          password = properties[passwordProperty]
       )
     }
+
+    const val DEFAULT_USERNAME_PROPERTY: String = "SONATYPE_NEXUS_USERNAME"
+    const val DEFAULT_PASSWORD_PROPERTY: String = "SONATYPE_NEXUS_PASSWORD"
   }
 }
