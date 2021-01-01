51ae7a5944255bfeadb980b71c2303424e55530e
Drew Hamilton <drew.hamilton.0@gmail.com>
2021-01-01 18:01:38 +0100

    Update MavenCoordinatesTest and StagingProfileRepositoryTest

--- src/test/kotlin/nevam/nexus/StagingProfileRepositoryTest.kt
+++ src/test/kotlin/nevam/nexus/StagingProfileRepositoryTest.kt
@@ -1,6 +1,8 @@
 package nevam.nexus
 
 import com.google.common.truth.Truth.assertThat
+import nevam.MavenCoordinates
+import nevam.Pom
 import org.junit.Test
 
 class StagingProfileRepositoryTest {
@@ -50,5 +52,20 @@ class StagingProfileRepositoryTest {
       |│ 2 │ cage.nicolas │ cagenicolas_1207 │ Open   │ Sun Aug 02 11:23:52 UTC 2020 │
       |└───┴──────────────┴──────────────────┴────────┴──────────────────────────────┘
       |""".trimMargin())
+  }
+
+  @Test fun contentUrlLinksToMavenGroup() {
+    val coordinates = MavenCoordinates("com.example", "nicolascage", "4.2.0")
+    val pom = Pom(coordinates)
+    val repo = StagingProfileRepository(
+      id = "cagenicolas_1206",
+      type = "closed",
+      isTransitioning = false,
+      updatedAtString = "Tue Aug 04 01:17:19 UTC 2020",
+      profileId = "9000",
+      profileName = "cage.nicolas"
+    )
+    assertThat(repo.contentUrl(pom))
+        .isEqualTo("https://oss.sonatype.org/content/repositories/${repo.id}/${coordinates.mavenGroupDirectory()}/")
   }
 }
