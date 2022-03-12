c8498dc4b9e3527fe6d22834eb52342af0f4967f
Saket <saket@squareup.com>
2022-03-12 00:48:14 -0500

    Remove an unused function in Pom

--- src/main/kotlin/nevam/Pom.kt
+++ src/main/kotlin/nevam/Pom.kt
@@ -4,9 +4,4 @@ data class Pom(val coordinates: MavenCoordinates) {
   val groupId: String get() = coordinates.groupId
   val artifactId: String get() = coordinates.artifactId
   val version: String get() = coordinates.version
-
-  /** e.g., app/cash/paparazzi/paparazzi/0.0.1 */
-  fun mavenDirectory(includeVersion: Boolean): String {
-    return coordinates.mavenDirectory(includeVersion)
-  }
 }
